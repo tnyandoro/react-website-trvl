34610a5015b4fb0312fca8268f678a681c7dcb5a
Tendai <tendai1@hotmail.com>
2021-08-20 23:54:07 +0200

    Add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,9 @@ function Navbar() {
     <div>
       <nav className="navbar">
         <div className="navbar-container">
-          
+          <Link to="/" className="navbar-logo">
+          TRVL
+          </Link>
         </div>
       </nav>
     </div>
