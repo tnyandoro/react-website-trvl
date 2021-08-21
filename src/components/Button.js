df594b396b976d9bdc6f44c5a26c33619aa1b9b6
Tendai <tendai1@hotmail.com>
2021-08-21 12:10:12 +0200

    return the sign-up button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import './Button.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
 
-const STYLES = ['btn--primary', 'btn--outline']
+const STYLES = ["btn--primary", "btn--outline"];
 
-const SIZES = ['btn--medium', 'btn--large']
+const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
-  buttonSize
+  buttonSize,
 }) => {
-const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.INCLUDES(buttonSize) 
+    ? buttonSize 
+    : SIZES[0];
+
+    return (
+      <Link to='/sign-up' className='btn-mobile'>
+        <button>
+          {children}   
+        </button>
+      </Link>
+    )
 };
