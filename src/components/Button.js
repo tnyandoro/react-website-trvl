6b5f31016a1387e0be7c289e664941cc150f0ee3
Tendai <tendai1@hotmail.com>
2021-08-21 12:02:52 +0200

    Add all styles for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
+const STYLES = ['btn--primary', 'btn--outline']
+
+const SIZES = ['btn--medium', 'btn--large']
+
 export const Button = ({
   children,
   type,
