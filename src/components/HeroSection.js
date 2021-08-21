1ffa89a0f4790bf9d5a2e617601c33ab395b42d7
Tendai Nyandoro <tendai1@hotmail.com>
2021-08-21 22:49:18 +0200

    Add the hero section text

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src='/video/video-2.mp4' autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      
+    </div>
+  )
+}
+
+export default HeroSection
