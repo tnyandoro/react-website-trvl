186c9e36cd4bd5f26da213f45c82c7f566757a30
Tendai <tendai1@hotmail.com>
2021-08-21 11:56:32 +0200

    Set button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import './Button.css';
+import { Link } from 'react-router-dom';
 
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize
+}) => {
+const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+};
