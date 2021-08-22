f6cbce645956434279dc870ed1cb3ea502f77759
Tendai <tendai1@hotmail.com>
2021-08-22 23:22:44 +0200

    Add page images

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
-import Services from './components/pages/Home';
-import Products from './components/pages/Home';
-import SignUp from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-
+import React from 'react'
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
