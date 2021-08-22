ed4c65329054798e55daa49896d5a6f4a1da01db
Tendai <tendai1@hotmail.com>
2021-08-22 22:38:55 +0200

    Add images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,6 +15,32 @@ function Cards() {
               label='Adventure'
               path='/services'
             />
+            <CardItem 
+              src='./images/img-2.jpg'
+              text='Inyanga hs the most beautiful forestry in the Zimbabwe'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+           <ul className="cards__items">
+            <CardItem 
+              src='./images/img-9.jpg'
+              text='Explore the most famous waterfall deep down in the Zimbabwe'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem 
+              src='./images/img-2.jpg'
+              text='Inyanga hs the most beautiful forestry in the Zimbabwe'
+              label='Luxury'
+              path='/services'
+            />
+            <CardItem 
+              src='./images/img-2.jpg'
+              text='Inyanga hs the most beautiful forestry in the Zimbabwe'
+              label='Luxury'
+              path='/services'
+            />
           </ul>
         </div>
       </div>
