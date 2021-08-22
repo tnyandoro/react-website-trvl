c8a02f7047f737be606fd2aabad0477df2983e14
Tendai <tendai1@hotmail.com>
2021-08-22 19:09:01 +0200

    add carditem component

---
+++ src/components/CardItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function CardItem() {
+  return (
+    <>
+    <li className="cards_items">
+      <Link className="cards_items_link">
+        
+      </Link>
+    </li>
+      
+    </>
+  )
+}
+
+export default CardItem;
