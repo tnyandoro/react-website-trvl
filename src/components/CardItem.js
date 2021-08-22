a2d2fed97c18014a890058bfd96fb4e7051a40e8
Tendai <tendai1@hotmail.com>
2021-08-22 21:12:23 +0200

    Add props

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function CardItem() {
+function CardItem(props) {
   return (
     <>
     <li className="cards__item">
-      <Link className="cards__item_link">
-        <figure className="cards__item__pic-wrap">
-          <img src="" alt="Travel " className="cards__item__img" />
+      <Link className="cards__item_link" to={props.path}>
+        <figure className="cards__item__pic-wrap" data-category={props.label}>
+          <img src={props.src} alt="Travel " className="cards__item__img" />
           <div className="cards__item__info">
             <h5 className="cards__item__text">
 
